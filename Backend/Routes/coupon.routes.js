5d5a925eedd1e2fd389806fd819ff0345cf15215
soni-shivam7544 <sonishivam7544@gmail.com>
2026-03-16 03:44:26 +0530

    Feat: Build Coupon Provider model.

--- Backend/Routes/coupon.routes.js
+++ Backend/Routes/coupon.routes.js
@@ -2,19 +2,19 @@ const couponController = require('../Controllers/coupon.controllers.js');
 
 const couponRouter = ( app ) => {
     // Create a new coupon
-    app.post('/coupons', couponController.createCoupon);
+    app.post('/cb/v1/api/coupons', couponController.createCoupon);
 
     // Get all coupons
-    app.get('/coupons', couponController.getAllCoupons);
+    app.get('/cb/v1/api/coupons', couponController.getAllCoupons);
 
     // Get a coupon by Id
-    app.get('/coupons/:id', couponController.getCouponById);
+    app.get('/cb/v1/api/coupons/:id', couponController.getCouponById);
 
     // Update a coupon by Id
-    app.put('/coupons/:id', couponController.updateCouponById);
+    app.put('/cb/v1/api/coupons/:id', couponController.updateCouponById);
 
     // Delete a coupon by Id
-    app.delete('/coupons/:id', couponController.destroyCoupon);
+    app.delete('/cb/v1/api/coupons/:id', couponController.destroyCoupon);
     
 }
 
