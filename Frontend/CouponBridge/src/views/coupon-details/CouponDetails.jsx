7b923b8a80f226db2dbad2028c373685f439efd8
ajaykumar0210 <ajaykumar021043@gmail.com>
2026-03-24 10:30:29 +0530

    Feature: Created View Page for getting coupon-details.

---
+++ Frontend/CouponBridge/src/views/coupon-details/CouponDetails.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import Main from './Main.jsx';
+
+const CouponDetails = () => {
+  return (
+    <>
+      <Navbar/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default CouponDetails
