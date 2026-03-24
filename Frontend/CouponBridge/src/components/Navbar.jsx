0edb156bca323afab6070dd9c11e6cd1537cb6a1
ajaykumar0210 <ajaykumar021043@gmail.com>
2026-03-24 10:29:25 +0530

    Feature: Added navigation to browse coupons page.

--- Frontend/CouponBridge/src/components/Navbar.jsx
+++ Frontend/CouponBridge/src/components/Navbar.jsx
@@ -3,17 +3,21 @@ import '../index.css';
 import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import SellIcon from '@mui/icons-material/Sell';
+import { useNavigate } from 'react-router-dom';
+
 function Navbar() {
+    
+    const navigate = useNavigate();
     return (
         <>
             <div className = "nav">
                 <div className = "nav-items heading">
-                    <div class="logo">
+                    <div className="logo" onClick={ () => navigate('/')}> 
                         <SellIcon color="primary" className="logo-icon" fontSize="large"/>
                         <div className="logo-name lg-heading">CouponBridge</div>
                     </div>
                     <div className="menu-items sub-heading">
-                        <Button variant="text" className="browse-coupons">Browse Coupons</Button>
+                        <Button variant="text" className="browse-coupons" onClick={ () => navigate('/coupons')}>Browse Coupons</Button>
                         <Button variant="text" className="merchants">Merchants</Button>
                         <Button variant="text" className="providers">Providers</Button>
                         <Button variant="text" className="how-it-works">How it works</Button>
