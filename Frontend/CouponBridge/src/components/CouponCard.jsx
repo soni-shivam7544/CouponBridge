0e645953b06610e6991feca2e3f2fd7232d4aa2c
soni-shivam7544 <sonishivam7544@gmail.com>
2026-04-15 14:19:50 +0530

    Update-Frontend: Updated the coupon section styling and coupon card styling.

--- Frontend/CouponBridge/src/components/CouponCard.jsx
+++ Frontend/CouponBridge/src/components/CouponCard.jsx
@@ -1,59 +1,61 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import DeleteIcon from '@mui/icons-material/Delete';
 import TimerIcon from '@mui/icons-material/Timer';
+import StarIcon from '@mui/icons-material/Star';
 import Button from '@mui/material/Button';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import VerifiedIcon from '@mui/icons-material/Verified';
+
+import watchImage from '../assets/images/watch.png';
 import '../index.css';
 import './CouponCard.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useState } from 'react';
 
 function CouponCard( { data } ) {
 
     const navigate = useNavigate();
-    const handleClick = () => {
-        axios.delete(`http://localhost:5050/cb/v1/api/coupons/${data._id}`)
-        .then( res => {
-            console.log("Deleted coupon successfully.");
-            navigate('/coupons');
-        })
-        .catch( err => console.log("Error is :", err));
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleLike = (e) => {
+        e.stopPropagation();
+        setIsLiked((like)=>(!like));
     }
 
     return (
         
-        <>
-            <div className="card" onClick={ ()=> navigate(`/coupons/${data._id}`)}>
-                <div className="card-header">
-                    <div className="card-title">
-                        <div className="card-logo lg-heading">{data.merchant[0].toUpperCase()}</div>
-                        <div className="card-subtitle heading">{data.merchant}</div>
+        <div className="coupon-card" onClick={ ()=> navigate(`/coupons/${data._id}`)}>
+            <div className="coupon-card-image">
+                <img src={watchImage} alt='image'/>
+                <div className='text coupon-card-discount'>20% OFF</div>
+                <div className='text coupon-card-isverified'><VerifiedIcon sx={{fontSize:'1.2rem', marginRight: '0.2rem'}}/><span>Verified</span></div>
+                <span className='coupon-card-like' onClick={handleLike}>{ !isLiked ? <FavoriteBorderIcon sx={{ color:'var(--color-text-muted)'}}/> : <FavoriteIcon sx={{ color:'var(--color-danger)'}}/>}</span>
+            </div>
+            <div className="coupon-card-info text">
+                <p style={{color:'var(--color-primary)'}}>Amazon</p>
+                <p className='heading'>20% Off Electronics</p>
+                <div className="coupon-card-provider caption">
+                    <p>Jane Smith</p>
+                    <div className="coupon-card-provider-rating">
+                        <StarIcon sx={{color: 'var(--color-highlight-hover)', fontSize:'1rem'}}/>
+                        <span>4.8</span>
                     </div>
-                    <div className="card-clicks">
-                        <FavoriteBorderIcon sx={{cursor: 'pointer', marginRight: '0.5rem'}}/>
-                        <DeleteIcon sx={{color:'red', cursor: 'pointer'}} onClick={ handleClick }/>
-                    </div>
-
                 </div>
-                <span className='card-title-discount heading'> <i> {data.discount}% Discount</i></span>
-                <div className="card-body sub-heading"> {data.description} </div>
-                <div className="card-footer">
-                    <div className="card-footer-details">
-                        <div className="price lg-heading">Rs. {data.price}/-</div>
-                        <div className="expiry sub-heading">
-                            <TimerIcon/>
-                            <p>30d left</p>
-                        </div>
+                <div className="coupon-card-expiry caption">
+                    <TimerIcon sx={{fontSize:'1rem'}}/>
+                    <span> 83 days left</span>
+                </div>
+                <div className="coupon-card-navigation">
+                    <div className="coupon-card-price lg-heading">
+                        <CurrencyRupeeIcon sx={{fontSize:'1.5rem'}}/>
+                        <span>25</span>
                     </div>
-                    <div className="card-footer-navigate sub-heading">
-                        <Button variant="contained" className='add-to-cart'>
-                            <AddShoppingCartIcon/>
-                            Add to Cart
-                        </Button>
-                    </div>
+                    <Button variant="contained"><AddShoppingCartIcon sx={{fontSize:'1.2rem', marginRight: '0.2rem'}}/><span>Add to Cart</span></Button>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
