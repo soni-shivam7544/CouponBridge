9de46f8561469e40e8c67509316043dbd95a6139
soni-shivam7544 <sonishivam7544@gmail.com>
2026-04-22 14:57:04 +0530

    Feature-Backend: Created Orders model.

---
+++ Backend/Models/order.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+    items:[{
+        coupon: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Coupon'
+        },
+        quantity:{
+            type: Number,
+            default: 1
+        }
+    }],
+    subtotal: Number,
+    platformFee: Number,
+    status: {
+        type: String,
+        enum:{
+            values: ['pending','paid','failed'],
+            message: 'Order status not valid!',
+            default: 'pending'
+        }
+    }
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
