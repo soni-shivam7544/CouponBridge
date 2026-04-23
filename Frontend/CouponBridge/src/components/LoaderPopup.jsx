88863dda9830332b926f08abea853f1699b75275
soni-shivam7544 <sonishivam7544@gmail.com>
2026-04-23 20:22:04 +0530

    Feature-Frontend: Created loader popup component.

---
+++ Frontend/CouponBridge/src/components/LoaderPopup.jsx
@@ -0,0 +1,16 @@
+import './LoaderPopup.css';
+
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+function LoaderPopup() {
+    return(
+        <div className='popup-overlay'>
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress aria-label="Loading…"/>
+            </Box>
+        </div>
+    )
+}
+
+export default LoaderPopup;
