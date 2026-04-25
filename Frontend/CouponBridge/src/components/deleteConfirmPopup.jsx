b0ad286c67818a1d31f6c6591f1a1f72c9ba82be
soni-shivam7544 <sonishivam7544@gmail.com>
2026-04-25 21:26:42 +0530

    FEature-Frontend: Updated DeleteConfirmpop for dynamic message popup.

--- Frontend/CouponBridge/src/components/deleteConfirmPopup.jsx
+++ Frontend/CouponBridge/src/components/deleteConfirmPopup.jsx
@@ -1,15 +1,17 @@
 import './Popup.css';
+import { usePopup } from '../hooks/usePopup';
 
 import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';import Button from '@mui/material/Button';
 
 const DeleteConfirmPopup = ({ onConfirm, onCancel }) => {
+  const {popup} = usePopup();
   return (
     <div className='popup-overlay'>
       <div className="popup-content">
         <WarningAmberOutlinedIcon sx={{fontSize:'4rem', color: 'var(--color-danger)', backgroundColor: 'var(--color-bg)', padding: '1rem', borderRadius: '50%'}}/>
         <p className='lg-heading' style={{color:'var(--color-danger)', margin: '0.5rem 0rem'}}>Confirm Delete!</p>
         <p className='sub-heading'>This action can not be undone.</p>
-        <p className='sub-heading'> All your data will be deleted permanantly!</p>
+        <p className='sub-heading'>{popup.data.message || "All your data will be deleted permanently!"}</p>
         <div className="popup-action" style={{margin: '1.5rem 0rem 0rem'}}>
             <Button variant="contained" sx={{backgroundColor:'var(--color-danger)', ":hover":{backgroundColor:'var(--color-danger-hover)'}}} onClick={onConfirm}>Delete</Button>
             <Button variant="contained"sx={{backgroundColor:'var(--color-btn-secondary-bg)', color: 'var(--color-btn-secondary-text)'}} onClick={onCancel}>Cancel</Button>
