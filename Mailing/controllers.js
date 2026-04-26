092a98caceaf47088944449ef16918753c868244
soni-shivam7544 <sonishivam7544@gmail.com>
2026-04-26 23:58:37 +0530

    Feature-Mailings: Created controllers.js.

---
+++ Mailing/controllers.js
@@ -0,0 +1,33 @@
+const services = require('./services');
+
+const composeNewMail = async (req, res) => {
+    try{
+        const response = await services.sendMail(req.body);
+        return res.status(201).json({
+            success: true,
+            data: response,
+            err: {},
+            message: "Sent mail successfully."
+        })
+    }catch(error) {
+        console.log(error);
+        if(error.err){
+            return res.status(error.code).json({
+                success: false,
+                data:{},
+                err: error.err,
+                message: "Mail could not be send."
+            })
+        }
+        return res.status(500).json({
+            success: false,
+            data:{},
+            err: error.err,
+            message: "Something went wrong."
+        })
+    }
+}
+
+module.exports = {
+    composeNewMail
+}
