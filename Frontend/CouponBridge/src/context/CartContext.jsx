aab420ef949d5fd74f8e61ff6481f2958d97847b
soni-shivam7544 <sonishivam7544@gmail.com>
2026-04-29 19:48:55 +0530

    Feature-Frontend: Created Cart Context.

---
+++ Frontend/CouponBridge/src/context/CartContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+
+import axios from 'axios';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children })=>{
+    const [cartCount, setCartCount] = useState(0);
+
+    const fetchCartCount = () => {
+        axios.get('http://localhost:5050/cb/v1/api/cart', {
+            headers: {
+                authorization: localStorage.getItem('token')
+            }
+        }).then(res => {
+            setCartCount(res.data.data.items.length);
+        }).catch(err => console.log(err.response));
+
+        
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:5050/cb/v1/api/cart', {
+            headers:{
+                authorization: localStorage.getItem('token')
+            }
+        }).then( res => {
+            console.log(res.data.data);
+            setCartCount(res.data.data.items.length);
+        }).catch(err => console.log(err.response));
+    },[]);
+
+    return(
+        <CartContext.Provider value = {{ cartCount, fetchCartCount }}>
+            { children }
+        </CartContext.Provider>
+    );
+
+}
